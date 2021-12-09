7ad7e12f8240f4e7f5f63ae849565e265a8f9e71
brown <ohbass95@gmail.com>
2021-12-09 18:19:20 +0900

    접근제어자04

---
+++ inner/ClassAccessModifierInnerPackage.java
@@ -0,0 +1,7 @@
+package org.opentutorials.javatutorials.accessmodifier.inner;
+
+public class ClassAccessModifierInnerPackage {
+// 같은 패키지의 파일이기 때문에 default,public 전부 사용가능하다.
+		PublicClass publicClass = new PublicClass();
+		DefaultClass defaultClass = new DefaultClass();
+}
---
+++ outter/ClassAccessModifierOuterPackage.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.accessmodifier.outter;
+import org.opentutorials.javatutorials.accessmodifier.inner.*;
+public class ClassAccessModifierOuterPackage {
+
+	PublicClass publicClass = new PublicClass();
+	
+//	DefaultClass defaultClass = new DefaultClass();
+//	default클레스는 같은 패키지에서만 사용가능하기때문에 에러가 발생한다
+	
+}
